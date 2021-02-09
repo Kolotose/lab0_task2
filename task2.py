11c5d2805d1e065c4d1e0b552746550e14618fd2
Anton Mazourick <amazourick@gmail.com>
2021-02-09 22:10:25 +0200

    First commit

---
+++ task2.py
@@ -0,0 +1,19 @@
+"""
+Repository link https://github.com/Kolotose/lab0_task2.git
+"""
+
+def read_board(path: str):
+    """
+    Read game board file from path.
+    Return list of str.
+
+    >>> read_board("board.txt")
+    ["**** ****", "***1 ****", "**  3****", \
+     "* 4 1****", "     9 5 ", " 6  83  *", \
+     "3   1  **", "  8  2***", "  2  ****"]
+    """
+    with open(path, 'r', encoding='utf-8') as file:
+        board = file.readlines()
+    file.close()
+
+    return board

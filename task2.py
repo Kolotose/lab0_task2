015942933601738fd5f5b1412344d7dde64693c1
Anton Mazourick <amazourick@gmail.com>
2021-02-09 22:17:31 +0200

    Fourth commit

--- task2.py
+++ task2.py
@@ -53,3 +53,39 @@ def check_board_size(board:list):
             return False
 
     return True
+
+def check_board_form(board: list):
+    """
+    Used in validate_board()
+
+    Checks if the board is correct form
+
+    >>> check_board_form(["**** ****", "***1 ****", "**  3****", \
+                          "* 4 1****", "     9 5 ", " 6  83  *", \
+                          "3   1  **", "  8  2***", "  2  ****"])
+    True
+    """
+    # Checking if stars are not placed where they do not need to be
+    for i in range(5):
+        # 5, because we have to have (8, 0), (7, 1), ..., (4, 4) be filled
+        # and it is 5 cells. We will be leaning on them (shape similar to L)
+        if board[8-i][i] == '*':
+            return False
+        
+        for j in range(1, 5):
+            if board[8-i-j][i] == '*' or board[8-i][i+j] == '*':
+                return False
+
+    # Checking if the tiles are not placed where they do not need to be
+    for i in range(4):
+        # Top right square
+        for j in range(4):
+            if board[i][8-j] != '*':
+                return False
+        
+        # Top left and bottom right square
+        for j in range(4-i):
+            if board[i][j] != '*' or board[8-i][8-j] != '*':
+                return False
+
+    return True

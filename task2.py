34608ec801433bae14d56f7336c91ebded4b542e
Anton Mazourick <amazourick@gmail.com>
2021-02-09 22:14:24 +0200

    second commit

--- task2.py
+++ task2.py
@@ -17,3 +17,19 @@ def read_board(path: str):
     file.close()
 
     return board
+
+def check_board_complete(board: list):
+    """
+    Checks if the board is completed with all the numbers
+
+    >>> check_board_complete(["**** ****", "***1 ****", "**  3****", \
+                              "* 4 1****", "     9 5 ", " 6  83  *", \
+                              "3   1  **", "  8  2***", "  2  ****"])
+    False
+    """
+    for line in board:
+        for element in line:
+            if element == ' ':
+                return False
+
+    return True
